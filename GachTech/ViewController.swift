55c67c6e6d293be098bc4d962724e290c875966e
渡辺航太郎 <watanabekoutarou@ld11-watanabe-kotaro-no-macbook.local>
2019-03-31 10:46:41 +0900

    Initial Commit

---
+++ GachTech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GachTech
+//
+//  Created by 渡辺航太郎 on 2019/03/31.
+//  Copyright © 2019 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
